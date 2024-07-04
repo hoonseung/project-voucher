c44f60e9e6042ae1772ee33e17261c6fe7dae7ae
lsh <tmdgns4029@naver.com>
2024-07-04 23:00:38 +0900

    [상품권 발행, 폐기] 검증 validator 리팩토링

--- src/main/java/com/study/projectvoucher/domain/validator/VoucherDisableValidator.java
+++ src/main/java/com/study/projectvoucher/domain/validator/VoucherDisableValidator.java
@@ -9,7 +9,11 @@ public class VoucherDisableValidator {
 
 
     public void validate(VoucherEntity voucherPs, VoucherDisableV2Request disableV2Request){
-        // 발행자만 폐기 가능
+        // 발행자만 폐기 가능하도록 요청자 검증
+        상품권_사용_불가_처리_권한이_있는지_확인(voucherPs, disableV2Request);
+    }
+
+    private static void 상품권_사용_불가_처리_권한이_있는지_확인(VoucherEntity voucherPs, VoucherDisableV2Request disableV2Request) {
         if (!voucherPs.publishHistory().getRequestType().equals(disableV2Request.requestType())
                 || !voucherPs.publishHistory().getRequestId().equals(disableV2Request.requestId())){
             throw new IllegalArgumentException("사용 불가 처리 권한이 없는 상품권 입니다.");
--- src/main/java/com/study/projectvoucher/domain/validator/VoucherPublishValidator.java
+++ src/main/java/com/study/projectvoucher/domain/validator/VoucherPublishValidator.java
@@ -3,12 +3,17 @@ package com.study.projectvoucher.domain.validator;
 import com.study.projectvoucher.domain.contract.ContractEntity;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 
 @Component
 public class VoucherPublishValidator {
 
+
     public void validate(ContractEntity contractEntity) {
+        // 상품권 발행을 위한 계약 유효 기간 발행 확인
+        상품권_발행을_위한_계약_유효기간이_만료되었는지_확인(contractEntity);
+    }
+
+    private static void 상품권_발행을_위한_계약_유효기간이_만료되었는지_확인(ContractEntity contractEntity) {
         if (contractEntity.periodIsExpired()){
             throw new IllegalArgumentException("유효기간이 지난 계약입니다.");
         }
